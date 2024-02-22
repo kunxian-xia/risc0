919daadd3d2c4c366c7dd3513aa363a46e3e4c5b
kunxian-xia <xiakunxian130@gmail.com>
2024-02-22 23:15:21 +0800

    add ntt bench

---
+++ risc0/zkp/benches/ntt.rs
@@ -0,0 +1,46 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use risc0_core::field::{baby_bear::BabyBearElem, Elem};
+use risc0_zkp::core::hash::poseidon2::Poseidon2HashSuite;
+use rand::thread_rng;
+use std::any::type_name;
+use criterion::BatchSize;
+use risc0_zkp::hal::cpu::CpuHal;
+#[cfg(feature = "cuda")]
+use risc0_zkp::hal::cuda::CudaHalPoseidon2;
+use risc0_zkp::hal::Hal;
+
+fn bench_ntt_internal<H: Hal<Elem=BabyBearElem>>(c: &mut Criterion, h: H, log_n: usize, batch: usize) {
+    let rng = &mut thread_rng();
+    let polys = (0..(1 << log_n) * batch)
+        .map(|_| BabyBearElem::random(rng))
+        .collect::<Vec<_>>();
+
+    c.bench_function(
+        &format!("ntt_{}_{}_{}", type_name::<H>(), log_n, batch),
+        |b| {
+            b.iter_batched(
+                || h.copy_from_elem("a", &polys),
+                |buf| h.batch_interpolate_ntt(&buf, batch),
+                BatchSize::SmallInput,
+            );
+        },
+    );
+}
+
+fn bench_ntt(c: &mut Criterion) {
+    for log_n in [19, 20, 21, 22] {
+        for batch in [1, 5, 20, 50] {
+            bench_ntt_internal(
+                c,
+                CpuHal::new(Poseidon2HashSuite::new_suite()),
+                log_n,
+                batch,
+            );
+            #[cfg(feature = "cuda")]
+            bench_ntt_internal(c, CudaHalPoseidon2::new(), log_n, batch);
+        }
+    }
+}
+
+criterion_group!(benches, bench_ntt);
+criterion_main!(benches);
